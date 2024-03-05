c204aa383ab23d81cab2cd9144c24b02d4ea3a85
SAMAKAKI <nikitos.shorick@gmail.com>
2024-03-05 17:52:23 +0100

    create the app

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import styles from './Header.module.css'
+
+const Header = () => {
+  return (
+    <div className="w-full h-14">
+      <div className="flex h-full w-full items-center justify-between">
+        <div>
+          <h1 className='text-3xl font-bold font-rubik'>samakaki<span className='text-rose-500'>.</span></h1> 
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
